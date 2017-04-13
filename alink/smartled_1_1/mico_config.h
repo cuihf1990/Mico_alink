2f5802c8ff800effb7bf952f89e3d5b8fef0a2ae
dingqq <dingqq@mxchip.com>
2017-04-13 19:17:05 +0800

    update 1.1 sn

--- alink/smartled_1_1/mico_config.h
+++ alink/smartled_1_1/mico_config.h
@@ -37,7 +37,7 @@
 #define MANUFACTURER        "MXCHIP Inc."
 #define SERIAL_NUMBER       "20140606"
 #define PROTOCOL            "com.mxchip.basic"
-#define MICO_SN             "0000.0000.0007"
+#define MICO_SN             "0000.0000.0008"
 
 #define MICO_WLAN_CONFIG_MODE_TRIGGER           (1) /**< Enter wlan config mode if no available wlan. */
 #define CONFIG_MODE_TRIGGER_EASYLINK_BTN        (1) /**< Enter wlan config mode by Easylink button. */
